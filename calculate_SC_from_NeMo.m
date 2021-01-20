18483cbb8be065d81f96536887645d2adff5d8b9
Emily Olafson <emily.olafson@mail.mcgill.ca>
2021-01-19 16:16:18 -0800

    json

--- calculate_SC_from_NeMo.m
+++ calculate_SC_from_NeMo.m
@@ -1,42 +1,28 @@
 % calculate SC from NeMo outputs
 % predict observed (partial) FC from estimated SC (from NeMo Tool) and see if there is a shift over time in the signaling mechanism that explains the most variance
-% September 14th 2020
-
-studydir = pwd;
-
-% Load average healthy connectome & individual healthy connectomes.
-allref=load(strcat(pwd,'/subject_data/fs86_avg/allref_denom.mat'))
-allref=allref.allref_denom;
-for i=1:420
-    neg_idx{i}=find(allref{i}<0);
-end
-
-
-full3d=[];
-for i=1:420
-    full3d=cat(3, full3d, full(allref{i}));
-end
-
-avg_connectome=squeeze(mean(full3d,3));
+% Jan 19th 2021
 
 % Load subject chacoconn scores
 for i=1:23
-    tmp=load(strcat(studydir,'/subject_data/SUB', num2str(i), '_lesion_1mmMNI_fs86subj_mean_chacoconn.mat'));
+    tmp=load(strcat(studydir,'/subject_data/fs86_subj/SUB', num2str(i), '_lesion_1mmMNI_fs86subj_mean_chacoconn.mat'));
     structname=strcat('SUB', num2str(i), 'chacoconn');
-    all_patients_chacoconn{i}=tmp.(structname);
+    all_patients_chacoconn{i}=full(tmp.(structname));
 end
 
-
+for i=1:23
+    A=load(strcat(studydir,'/subject_data/fs86_subj/SUB', num2str(i), '_lesion1mm_nemo_output_chacoconn_fs86subj_allref_denom.mat'));
+    avg_connectome{i}=mean(A.allref,3)
+end
 %% Multiply subject chacoconn scores by avg healhy connectome to get
 % 'disrupted' connectome 
 for i=1:23
-    all_patients_disrupted{i}=all_patients_chacoconn{i}.*avg_connectome;
+    all_patients_disrupted{i}=all_patients_chacoconn{i}.*avg_connectome{i};
 end
 
 % Subtract 'disrupted' connectome from average healthy connectome to get
 % 'spared' connectome
 for i=1:23
-    all_patients_spared{i}=avg_connectome-all_patients_disrupted{i};
+    all_patients_spared{i}=avg_connectome{i}-all_patients_disrupted{i};
 end
 
 for i=1:23
